20e3e53ccea6ac34c53f92e6badc5ed84d8a0873
Chole_Liu <842007226@qq.com>
2016-10-09 16:56:50 +0800

    新增了环境与变量请求处理的文件

---
+++ AutoTest/platform/datahandle/envdata.py
@@ -0,0 +1,10 @@
+# 处理环境相关的请求
+"""
+@author:liujing
+"""
+
+from django.core import serializers
+import json
+
+from ...models import Env
+
---
+++ AutoTest/platform/datahandle/vardata.py
@@ -0,0 +1,10 @@
+# 处理全局变量相关的请求
+"""
+@author:liujing
+"""
+
+from django.core import serializers
+import json
+
+from ...models import Vars
+
