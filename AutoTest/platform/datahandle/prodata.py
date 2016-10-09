6d64a9224c0ff03fea84e53a4718526cc3fcf738
Chole_Liu <842007226@qq.com>
2016-10-09 15:58:26 +0800

    新增了首页关于项目模块的代码及url，注释等

--- AutoTest/platform/datahandle/prodata.py
+++ AutoTest/platform/datahandle/prodata.py
@@ -12,16 +12,25 @@ from ...models import Project
 def getProList():
     """
     获取所有的项目
-    :param data:前端的请求数据
     :return: 所有的项目，类型为列表
     """
     body = []
-    data = json.loads(serializers.serialize("json", Project.objects.all()))
-    for e in data:
-        str1 = e['fields']
-        str1['id'] = e['pk']
-        body.append(str1)
-    return body
+    try:
+        data = json.loads(serializers.serialize("json", Project.objects.all()))
+        for e in data:
+            str1 = e['fields']
+            str1['id'] = e['pk']
+            body.append(str1)
+        return {
+            "code": 1,
+            "msg": "获取成功",
+            "data": body
+        }
+    except Exception as e:
+        return {
+            "code": 0,
+            "msg": "获取失败",
+        }
 
 
 def getProDetail(data):
@@ -30,14 +39,24 @@ def getProDetail(data):
     :param data:
     :return:
     """
-    pro_id = data['pro_id']
-    data = Project.objects.all().get(id=pro_id)
-    data_json = {
-        "id": data.id,
-        "pro_name": data.pro_name,
-        "pro_desc": data.pro_desc
-    }
-    return data_json
+    try:
+        pro_id = data['pro_id']
+        data = Project.objects.all().get(id=pro_id)
+        data_json = {
+            "id": data.id,
+            "pro_name": data.pro_name,
+            "pro_desc": data.pro_desc
+        }
+        return {
+            "code": 1,
+            "msg": "获取成功",
+            "data": data_json
+        }
+    except Exception as e:
+        return {
+            "code": 0,
+            "msg": "获取失败，id不存在",
+        }
 
 
 def createPro(data):
@@ -46,22 +65,58 @@ def createPro(data):
     :param data:
     :return: 无
     """
-    name = data['pro_name']
-    description = data['pro_desc']
-    Project.objects.create(pro_name=name,pro_desc=description)
+    try:
+        name = data['pro_name']
+        description = data['pro_desc']
+        if name == '':
+            return {
+                "code": 0,
+                "msg": "项目名不能为空！"
+            }
+        Project.objects.create(pro_name=name, pro_desc=description)
+        return {
+                "code":1,
+                "msg": "项目创建成功"
+            }
+    except Exception as e:
+        print(e)
+        return {
+                "code":0,
+                "msg": "项目创建失败"
+            }
 
 
-def editSavePro(data):
+def editPro(data):
     """
     编辑项目
     :param data:
     :return: 无
     """
-    pro_id = data['pro_id']
-    pro = Project.objects.all().get(id=pro_id)
-    pro.pro_name = data['pro_name']
-    pro.pro_desc = data['pro_desc']
-    pro.save()
+    try:
+        pro_id = data['pro_id']
+        pro_name = data['pro_name']
+        pro_desc = data['pro_desc']
+        if pro_name == '':
+            return {
+                "code": 0,
+                "msg": "项目名不能为空！"
+            }
+        a = Project.objects.all().filter(id=pro_id).update(pro_name=pro_name,pro_desc=pro_desc)
+        if a == 0:
+            return {
+                "code": 0,
+                "msg": "修改失败，项目id不存在！"
+            }
+        return {
+            "code": 1,
+            "msg": "修改成功"
+        }
+    except Exception as e:
+        print(e)
+        return {
+            "code": 0,
+            "msg": "修改失败"
+        }
 
 
 def delPro(data):
@@ -70,6 +125,17 @@ def delPro(data):
     :param data:
     :return:无
     """
-    pro_id = data['pro_id']
-    pro = Project.objects.all().get(id=pro_id)
-    pro.delete()
+    try:
+        pro_id = data['pro_id']
+        pro = Project.objects.all().get(id=pro_id)
+        pro.delete()
+        return {
+            "code": 1,
+            "msg": "删除成功"
+        }
+    except Exception as e:
+        print(e)
+        return {
+            "code": 0,
+            "msg": "删除失败，项目id不存在"
+        }
--- AutoTest/urls.py
+++ AutoTest/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^$', views.to_index),
     url(r'project/list/$', views.pro_list),
     url(r'project/detail/$', views.pro_detail),
-    #url(r'project/save/$', views.pro_save),
-    # url(r'project/delete/$', views.pro_del),
+    url(r'project/create/$', views.pro_create),
+    url(r'project/edit/$', views.pro_edit),
+    url(r'project/delete/$', views.pro_del),
 ]
