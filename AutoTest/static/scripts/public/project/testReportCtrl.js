fee9d9831be4d1a5c28383e99210029d6be7154f
xuqing1993 <374127901@qq.com>
2016-11-12 18:57:22 +0800

    10.12   测试报告控制器

--- AutoTest/static/scripts/public/project/testReportCtrl.js
+++ AutoTest/static/scripts/public/project/testReportCtrl.js
@@ -15,42 +15,92 @@ myApp.controller('testReportCtrl',function($scope,$http,$cookieStore,$timeout) {
     $scope.allResult=[];
 
      $timeout(function(){
+         $scope.repDetail=false;
+         $scope.repList=true;
          $http.post('project/result/list',{
              "pro_id":pro_id
          }).success(function(response1){
              if(response1.code=1){
                  $scope.resultList=response1.data;
-                 $scope.result=$scope.resultList[0];
-                 $http.post('project/result/detailList',{
-                     "result_id":$scope.result.result_id
-                 }).success(function(response){
-                     if(response.code=1){
-                         $scope.allResult=response.data;
-                         $scope.resultStr="";
-                         if($scope.allResult.length>1){
-                             for(var i=0; i<$scope.allResult.length-1;i++){
-                                 var str= angular.toJson($scope.allResult[i]);
-                                 $scope.resultStr = $scope.resultStr + str + " ";
-                             }
-                             str= angular.toJson($scope.allResult[$scope.allResult.length-1]);
-                             $scope.resultStr=$scope.resultStr+str;
-                         }else{
-                             str= angular.toJson($scope.allResult[$scope.allResult.length-1]);
-                             $scope.resultStr=$scope.resultStr+str;
-                         }
-                     }else{
-                         alert(response1.msg)
-                     }
-                 })
-
-
-
              }else{
                  alert(response.msg)
              }
          })
      })
 
+    $scope.showDetail=function(obj){
+        $scope.result=obj;
+        $scope.repList=false;
+        $scope.repDetail=true;
+        $http.post('project/result/detailList',{
+             "result_id":obj.result_id
+        }).success(function(response){
+             if(response.code=1){
+                 $scope.allResult=response.data;
+                 $scope.resultStr="";
+                 if($scope.allResult.length>1){
+                     for(var i=0; i<$scope.allResult.length-1;i++){
+                         var str= angular.toJson($scope.allResult[i]);
+                         $scope.resultStr = $scope.resultStr + str + " ";
+                     }
+                     str= angular.toJson($scope.allResult[$scope.allResult.length-1]);
+                     $scope.resultStr=$scope.resultStr+str;
+                 }else{
+                     str= angular.toJson($scope.allResult[$scope.allResult.length-1]);
+                     $scope.resultStr=$scope.resultStr+str;
+                 }
+             }else{
+                 alert(response1.msg)
+             }
+        })
+    }
+
+    $scope.returnList=function(){
+         $scope.repDetail=false;
+         $scope.repList=true;
+         $http.post('project/result/list',{
+             "pro_id":pro_id
+         }).success(function(response1){
+             if(response1.code=1){
+                 $scope.resultList=response1.data;
+             }else{
+                 alert(response.msg)
+             }
+         })
+    }
+
+    /*
+    $scope.inquiry=function(str){
+        for (var i=0; i<$scope.resultList.length;i++){
+            if($scope.resultList[i].report_name==str){
+                $scope.result=$scope.resultList[i];
+            }
+            break;
+        }
+        console.log($scope.result)
+        $http.post('project/result/detailList',{
+            "result_id":$scope.result.result_id
+        }).success(function(response){
+            if(response.code=1){
+                $scope.allResult=response.data;
+                console.log($scope.allResult)
+                $scope.resultStr="";
+                if($scope.allResult.length>1){
+                    for(var i=0; i<$scope.allResult.length-1;i++){
+                        var str= angular.toJson($scope.allResult[i]);
+                        $scope.resultStr = $scope.resultStr + str + " ";
+                    }
+                    str= angular.toJson($scope.allResult[$scope.allResult.length-1]);
+                    $scope.resultStr=$scope.resultStr+str;
+                }else{
+                    str= angular.toJson($scope.allResult[$scope.allResult.length-1]);
+                    $scope.resultStr=$scope.resultStr+str;
+                }
+            }else{
+                alert(response.msg)
+            }
+        })
+    }
     $scope.active=function(id,obj){
         $scope.result=$scope.resultList[id];
         for(var i=0;i<$scope.resultList.length;i++){
@@ -78,7 +128,6 @@ myApp.controller('testReportCtrl',function($scope,$http,$cookieStore,$timeout) {
                 alert(response.msg)
             }
         })
-
-    }
+    }*/
 
 })
