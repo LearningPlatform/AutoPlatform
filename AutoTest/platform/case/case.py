391765aed18cc48416543c1f35613cdd24827639
Chole_Liu <842007226@qq.com>
2016-11-14 00:55:39 +0800

    使用类继承，抽象化接口数据的生成与请求

--- AutoTest/platform/case/case.py
+++ AutoTest/platform/case/case.py
@@ -1,92 +1,56 @@
-from ...models import Case, CaseSuite, Api, Result, ResultDetail, DepndApi
-from ..tools import strtool
+from ...models import Case, Api, ResultDetail
 from .dapi import Interface
 
-import requests
+from .req import ReqResp
+from ..tools import strtool
+
 import json
 
 
-class CaseEntity:
+class CaseEntity(ReqResp):
+
+    result_id = 0
+    result_detail = object
+    case = object
+    api = object
+    is_pass = 0
 
     def __init__(self, case_id, var_map, result_id):
-        self.case_id = case_id
-        self.var_map = var_map
-        self.url = ""
-        self.param = ""
-        self.case = object
-        self.api = object
-        self.resp = {}
-        self.is_pass = 0
+        ReqResp.__init__(self)
         self.result_id = result_id
         self.result_detail = ResultDetail.objects.create(result_id=self.result_id)
-        self.setCase()
-        self.selApi()
-        self.setUrl()
-        self.setReqParam()
-        self.sendRequest()
-        self.check_result()
-        self.result_detail.save()
-
-    def setCase(self):
-        self.case = Case.objects.all().get(case_id=self.case_id)
-        self.result_detail.case_id = self.case_id
-        self.result_detail.exp_data = self.case.exp_data
-        self.result_detail.case_desc = self.case.case_desc
-
-    def selApi(self):
+        self.case = Case.objects.all().get(case_id=case_id)
         self.api = Api.objects.all().get(api_id=self.case.api_id)
-        self.result_detail.api_id = self.api.api_id
-
-    def setUrl(self):
-        self.url = self.api.api_protocol + "://" + self.api.api_url
-        while "{{" in self.url:
-            a, b, str_param = strtool.str_replace(self.url, 1)
-            self.url = self.url.replace(self.url[a:b], self.var_map[str_param])
-
-    def setReqParam(self):
+        self.url = self.api.api_url
         self.param = self.case.input_data
-        while "{{" in self.param:
-            a, b, str_param = strtool.str_replace(self.param, 1)
-            self.param = self.param.replace(self.param[a:b], self.var_map[str_param])
+        self.api_protocol = self.api.api_protocol
+        self.depnd_api_id = self.case.depnd_api_id
+        self.api_method = self.api.api_method
+        self.var_map = var_map
+        self.handle_depnd_param()
+
+    def handle_depnd_param(self):
         while "$." in self.param:
             path = strtool.str_replace(self.param, 2)
-            self.d_api = Interface(self.case.depnd_api_id,var_map=self.var_map)
-            self.param = self.param.replace(path,str(self.d_api.get_param_value(path)))
-        self.param = self.param.replace("\'", "\"")
-        self.result_detail.input_data = self.param
-
-    def sendRequest(self):
-        if self.api.api_method == "post":
-            if "headers" in self.var_map.keys():
-                headers = json.loads(self.var_map["headers"])
-                re = requests.post(self.url, data=self.param, headers=headers)
-            else:
-                re = requests.post(self.url, data=self.param)
-            self.resp = {
-                "header": re.headers,
-                "status_code": re.status_code,
-                "content": re.json()
-            }
-        if self.api.api_method == "get":
-            if "headers" in self.var_map.keys():
-                headers = json.loads(self.var_map["headers"])
-                self.param = json.loads(self.param)
-                re = requests.get(self.url, params=self.param, headers=headers)
-            else:
-                re = requests.get(self.url, params=self.param)
-            self.resp = {
-                "header": re.headers,
-                "status_code": re.status_code,
-                "content": re.json()
-            }
-        self.result_detail.out_data = self.resp
+            self.d_api = Interface(self.depnd_api_id, var_map=self.var_map)
+            self.d_api.run()
+            self.param = self.param.replace(path, str(self.d_api.get_param_value(path)))
 
     def check_result(self):
         if json.dumps(self.resp["content"], ensure_ascii=False).find(self.case.exp_data) == -1:
             self.is_pass = 0
         else:
             self.is_pass = 1
+
+    def save_result(self):
+        self.result_detail.case_id = self.case.case_id
+        self.result_detail.exp_data = self.case.exp_data
+        self.result_detail.case_desc = self.case.case_desc
+        self.result_detail.api_id = self.api.api_id
+        self.result_detail.input_data = self.param
+        self.result_detail.out_data = self.resp
         self.result_detail.is_pass = self.is_pass
+        self.result_detail.save()
 
     def get_passnum(self):
         return self.is_pass
--- AutoTest/platform/case/dapi.py
+++ AutoTest/platform/case/dapi.py
@@ -1,64 +1,35 @@
 from ...models import DepndApi
+from .req import ReqResp
 from ..tools import strtool
 
-import json
-import requests
 
+class Interface(ReqResp):
+    case = object
+    depnd_api = object
 
-class Interface:
     def __init__(self, D_api_id, var_map):
+        ReqResp.__init__(self)
         self.depnd_api_id = D_api_id
-        self.var_map = var_map
-        self.depnd_api = object
-        self.url = ""
-        self.param = ""
-        self.resp = {}
-        self.pick_param = {}
-        self.setApi()
-        self.setUrl()
-        self.setReqParam()
-        self.sendRequest()
-
-    def setApi(self):
         self.depnd_api = DepndApi.objects.all().get(depnd_api_id=self.depnd_api_id)
-
-    def setUrl(self):
-        self.url = self.depnd_api.depnd_api_protocol + "://" + self.depnd_api.depnd_api_url
-        while "{{" in self.url:
-            a, b, str_param = strtool.str_replace(self.url, 1)
-            self.url = self.url.replace(self.url[a:b], self.var_map[str_param])
-
-    def setReqParam(self):
+        self.url = self.depnd_api.depnd_api_url
         self.param = self.depnd_api.depnd_api_param
-        while "{{" in self.param:
-            a, b, str_param = strtool.str_replace(self.param, 1)
-            self.param = self.param.replace(self.param[a:b], self.var_map[str_param])
+        self.api_protocol = self.depnd_api.depnd_api_protocol
+        self.depnd_api_id = self.depnd_api.depnd_id
+        self.api_method = self.depnd_api.depnd_api_method
+        self.var_map = var_map
+        self.handle_depnd_param()
+
+    def handle_depnd_param(self):
         while "$." in self.param:
             path = strtool.str_replace(self.param, 2)
-            self.d_api = Interface(self.depnd_api.depnd_id,var_map=self.var_map)
-            self.param = self.param.replace(path,str(self.d_api.get_param_value(path)))
-        self.param = self.param.replace("\'", "\"")
-
-    def sendRequest(self):
-        if self.depnd_api.depnd_api_method == "post":
-            if "headers" in self.var_map.keys():
-                headers = json.loads(self.var_map["headers"])
-                re = requests.post(self.url, data=self.param, headers=headers)
-            else:
-                re = requests.post(self.url, data=self.param)
-        if self.depnd_api.depnd_api_method == "get":
-            if "headers" in self.var_map.keys():
-                headers = json.loads(self.var_map["headers"])
-                self.param = json.loads(self.param)
-                re = requests.get(self.url, params=self.param, headers=headers)
-            else:
-                re = requests.get(self.url, params=self.param)
-        self.resp = re.json()
+            self.d_api = Interface(self.depnd_api_id, var_map=self.var_map)
+            self.d_api.run()
+            self.param = self.param.replace(path, str(self.d_api.get_param_value(path)))
 
     def get_param_value(self, param_path):
         param_path = param_path[2:]
         param_path = param_path.split('.')
-        param = self.resp
+        param = self.resp["content"]
         for p in param_path:
             param = param[p]
         return param
