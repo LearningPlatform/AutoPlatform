e88513b96848026273cdd2b1b6c3dc1a273334cc
Chole_Liu <842007226@qq.com>
2016-11-22 11:48:49 +0800

    1.增加对应字符替换的注释 2.注释掉没有用的方法（日后可能有用，所以未删除）

--- AutoTest/platform/tools/strtool.py
+++ AutoTest/platform/tools/strtool.py
@@ -15,11 +15,13 @@ def byteToStr(byte, encoding="utf-8"):
 
 
 def str_replace(str_re, str_type):
+    # 替换全局变量
     if str_type == 1:
         a = str_re.find('{{')
         b = str_re.find('}}')
         str1 = str_re[a + 2:b]
         return a, b+2, str1
+    # 替换依赖接口值
     if str_type == 2:
         a = str_re.find('$.')
         b_list = [str_re.find('"', a), str_re.find(',', a), str_re.find('}', a), str_re.find('\'', a)]
@@ -29,26 +31,27 @@ def str_replace(str_re, str_type):
                 b = i
         path = str_re[a:b]
         return path
+    # 替换常用方法返回值
     if str_type == 3:
         a = str_re.find('{$')
         b = str_re.find('$}')
         str1 = str_re[a + 2:b]
         str_func_name = str1[0:str1.find('(')]
         return a, b + 2, str1, str_func_name
-    if str_type == 4:
-        a = str_re.find('{$')
-        b = str_re.find('$}')
-        str1 = str_re[a + 2:b]
-        str_func_name = str1[0:str1.find('(')]
-        str_func_param = str1[str1.find('(') + 1:-1].split(',')
-        for index, param in enumerate(str_func_param):
-            if param.find("\'") != -1 or param.find("\"") != -1:
-                str_func_param[index] = str_func_param[index][1:-1]
-            else:
-                if param.find('.') != -1:
-                    str_func_param[index] = float(param)
-                else:
-                    str_func_param[index] = int(param)
-        return a, b + 2, str_func_name, str_func_param
+    # if str_type == 4:
+    #     a = str_re.find('{$')
+    #     b = str_re.find('$}')
+    #     str1 = str_re[a + 2:b]
+    #     str_func_name = str1[0:str1.find('(')]
+    #     str_func_param = str1[str1.find('(') + 1:-1].split(',')
+    #     for index, param in enumerate(str_func_param):
+    #         if param.find("\'") != -1 or param.find("\"") != -1:
+    #             str_func_param[index] = str_func_param[index][1:-1]
+    #         else:
+    #             if param.find('.') != -1:
+    #                 str_func_param[index] = float(param)
+    #             else:
+    #                 str_func_param[index] = int(param)
+    #     return a, b + 2, str_func_name, str_func_param
 
 
