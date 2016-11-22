813a1ce8597416f520708a27773af0e208593783
xuqing1993 <374127901@qq.com>
2016-11-22 09:29:45 +0800

    11.21  将选项保存

--- AutoTest/static/scripts/public/project/proCtrl.js
+++ AutoTest/static/scripts/public/project/proCtrl.js
@@ -1,10 +1,14 @@
  myApp.controller('proCtrl',function($scope,$http,$cookieStore,$timeout){
      var pro_id = $cookieStore.get("currProID");
+
      $scope.activeList=["active","disactive","disactive","disactive","disactive","disactive","disactive","disactive","disactive"];
 
     $scope.pro={};
 
+     var model_id=0;
+
      $timeout(function() {
+         model_id = $cookieStore.get("currmodelID");
          $http.post('project/detail', {
              "pro_id": pro_id
          }).success(function (response) {
@@ -13,10 +17,11 @@
          for (var i=0;i<9;i++){
              $scope.activeList[i]="disactive";
          }
-         $scope.activeList[0]='active';
+         $scope.activeList[model_id]='active';
      })
 
      $scope.active = function(id) {
+         $cookieStore.put("currmodelID",id);
          for (var i=0;i<9;i++){
              $scope.activeList[i]="disactive";
          }
