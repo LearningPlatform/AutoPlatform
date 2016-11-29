dd73865bd4a3ead8927b7622a341d7b21aa034b8
xuqing1993 <374127901@qq.com>
2016-11-29 09:25:01 +0800

    11.29

--- AutoTest/static/scripts/public/project/recordCtrl.js
+++ AutoTest/static/scripts/public/project/recordCtrl.js
@@ -1,66 +1,62 @@
 myApp.controller('recordCtrl', function ($scope, $http, $cookieStore, $timeout) {
     var pro_id = $cookieStore.get("currProID");
-    var dataSocket;
     $scope.record_info={
-        host:"192.168.1.102",
+        host:"10.170.18.55",
         port:8003,
         filter:""
     }
-    $scope.servers_url = ""
-    $scope.reqData=[]
-    $scope.showiframe = false
+    $scope.servers_url = "";
+    $scope.reqData=[];
+    $scope.showiframe = false;
+    var dataSocket;
+    var baseUrl;
+    var socketPort;
 
     $scope.dfRecord=function(){
         $("#recordModal").modal();
     }
 
-    $scope.startRecord = function () {
-        var baseUrl = $scope.record_info.host,
-            socketPort = $scope.record_info.port;
+    $scope.initSocket=function () {
+        //self.bodyCbMap = {};
+        dataSocket = new WebSocket("ws://" + baseUrl + ":" + socketPort);
+        dataSocket.onmessage = function (event) {
+            data = JSON.parse(event.data);
+            /*type = data.type;
+            $scope.content = data.content;
+            reqRef = data.reqRef;
+            if (type == "update") {
 
-        function initSocket() {
-            self.bodyCbMap = {};
-            dataSocket = new WebSocket("ws://" + baseUrl + ":" + socketPort);
-            dataSocket.onmessage = function (event) {
-                data = JSON.parse(event.data),
-                        type = data.type,
-                        content = data.content,
-                        reqRef = data.reqRef;
-                if (type == "update") {
-
-                } else if (type == "body") {
-
-                    if (data.reqRef && self.bodyCbMap[reqRef]) {
-                        self.bodyCbMap[reqRef].call(self, content);
-                    }
+            } else if (type == "body") {
+                if (data.reqRef && self.bodyCbMap[reqRef]) {
+                    self.bodyCbMap[reqRef].call(self, content);
                 }
-                $scope.reqData.push(data);
-                console.log($scope.reqData)
-            }
-            self.dataSocket = dataSocket;
-
+            }*/
+            $scope.reqData.push(data);
         }
+        //self.dataSocket = dataSocket;
+    }
 
-        initSocket();
+    $scope.startRecord = function () {
+        baseUrl = $scope.record_info.host;
+        socketPort = $scope.record_info.port;
+        $scope.initSocket();
         $("#recordModal").modal('hide');
-        $scope.servers_url = "http://" + $scope.record_info.host+":"+"8002/"
+        $scope.servers_url = "http://" + $scope.record_info.host+":"+"8002/";
         $("iframe").attr("src",$scope.servers_url);
-
     }
 
     $scope.stopRecord = function () {
-        dataSocket.close()
+        dataSocket.close();
     }
 
     $scope.showRecord = function () {
-
-        $scope.showtable = false
-        $scope.showiframe = true
+        $scope.showtable = false;
+        $scope.showiframe = true;
     }
 
     $scope.setRecord = function () {
-        $scope.showiframe = false
-        $scope.showtable = true
+        $scope.showiframe = false;
+        $scope.showtable = true;
     }
 
 })
