56fac2e466f606533ee04fea69522ad808ff31d1
Chole_Liu <842007226@qq.com>
2016-11-30 00:47:55 +0800

    获取响应值

--- AutoTest/static/scripts/public/project/recordCtrl.js
+++ AutoTest/static/scripts/public/project/recordCtrl.js
@@ -1,9 +1,9 @@
 myApp.controller('recordCtrl', function ($scope, $http, $cookieStore, $timeout) {
     var pro_id = $cookieStore.get("currProID");
     $scope.record_info={
-        host:"10.170.56.122",
+        host:"192.168.1.102",
         port:8003,
-        filter:"url"
+        filter:"supernano"
     }
     $scope.servers_url = "";
     $scope.reqData=[];
@@ -22,6 +22,7 @@ myApp.controller('recordCtrl', function ($scope, $http, $cookieStore, $timeout) 
     var reqLength;
     var data;
     var reqData_str;
+    var i = 0
     $scope.initSocket=function () {
         dataSocket = new WebSocket("ws://" + baseUrl + ":" + socketPort);
         dataSocket.onmessage = function (event) {
@@ -29,16 +30,21 @@ myApp.controller('recordCtrl', function ($scope, $http, $cookieStore, $timeout) 
             reqData_str = JSON.stringify(data);
             if(reqData_str.indexOf($scope.record_info.filter)!=-1){
                 data.content.reqHeader = JSON.stringify(data.content.reqHeader)
-                $scope.reqData.push(data.content);
-            }
-            reqLength=$scope.reqData.length;
-            for (var i=0;i<reqLength;i++){
-                $scope.styleList.push($scope.tableStyle);
-                reqURL="http://10.170.51.96:8002/fetchBody?id="+$scope.reqData[i].id;
-                $http.jsonp(reqURL).success(function(response){
-                    //response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1");
-                    $scope.reqData.push(response.content);
+                temp = data.content
+                temp_resp = ""
+                $http.post("project/record/reqdetail",{
+                    host:$scope.record_info.host,
+                    port:8002,
+                    req_id:temp.id
+                }).success(function (response) {
+                    if (typeof(response.content) != "undefined") {
+                        temp_resp = response.content
+                    }
+                    temp["resBody"] = temp_resp
+
                 })
+            $scope.reqData.push(temp);
+
             }
         }
     }
