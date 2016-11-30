0d4f8c7ce35ce792697455264f6ebf8944c3c09e
Chole_Liu <842007226@qq.com>
2016-11-30 16:38:32 +0800

    增加手动输入case类与录制case类的处理

---
+++ AutoTest/platform/case/mcase.py
@@ -0,0 +1,43 @@
+from ...models import Case, Api, ResultDetail
+
+from .req import ReqResp
+from .case import CaseEntity
+
+
+class MCase(CaseEntity, ReqResp):
+
+    case = object
+
+    def __init__(self, case_id, var_map, result_id):
+        ReqResp.__init__(self)
+        self.result_id = result_id
+        self.result_detail = ResultDetail.objects.create(result_id=self.result_id)
+        self.case = Case.objects.all().get(case_id=case_id)
+        self.api = Api.objects.all().get(api_id=self.case.api_id)
+        self.url = self.api.api_url
+        self.param = self.case.input_data
+        self.api_protocol = self.api.api_protocol
+        self.depnd_api_id = self.case.depnd_api_id
+        self.api_method = self.api.api_method
+        self.var_map = var_map
+        self.handle_depnd_param()
+        self.pro_id = self.case.pro_id
+        self.url = self.api_protocol + "://" + self.url
+        self.exp_data = self.case.exp_data
+        self.resp_type = self.api.api_type
+
+    def save_result(self):
+        CaseEntity.save_result(self)
+        input_data = {
+            "url": self.url,
+            "body": self.param
+        }
+        self.result_detail.case_id = self.case.case_id
+        self.result_detail.exp_data = self.case.exp_data
+        self.result_detail.case_desc = self.case.case_desc
+        self.result_detail.api_id = self.api.api_id
+        self.result_detail.input_data = input_data
+        self.result_detail.out_data = self.resp
+        self.result_detail.is_pass = self.is_pass
+        self.result_detail.case_type = 1
+        self.result_detail.save()
---
+++ AutoTest/platform/case/rcdcase.py
@@ -0,0 +1,42 @@
+from ...models import RecordCase, ResultDetail
+
+from .case import CaseEntity
+
+
+class RcdCase(CaseEntity):
+
+    rcd_case = object
+
+    def __init__(self, case_id, var_map, result_id):
+        CaseEntity.__init__(self)
+        self.result_id = result_id
+        self.result_detail = ResultDetail.objects.create(result_id=self.result_id)
+        self.rcd_case = RecordCase.objects.all().get(rcd_case_id=case_id)
+        self.url = self.rcd_case.rcd_case_url
+        self.param = self.rcd_case.rcd_case_input_data
+        self.depnd_api_id = self.rcd_case.depnd_api_id
+        self.api_method = self.rcd_case.rcd_case_method
+        self.var_map = var_map
+        self.handle_depnd_param()
+        self.pro_id = self.rcd_case.pro_id
+        self.exp_data = self.rcd_case.rcd_case_exp_data
+        self.resp_type = self.rcd_case.resp_type
+
+    def save_result(self):
+        CaseEntity.save_result(self)
+        input_data = {
+            "url": self.url,
+            "body": self.param
+        }
+        self.result_detail.case_id = self.rcd_case.rcd_case_id
+        self.result_detail.exp_data = self.rcd_case.rcd_case_exp_data
+        self.result_detail.case_desc = self.rcd_case.rcd_case_desc
+        self.result_detail.api_id = self.rcd_case.api_id
+        self.result_detail.input_data = input_data
+        self.result_detail.out_data = self.resp
+        self.result_detail.is_pass = self.is_pass
+        self.result_detail.case_type = 2
+        self.result_detail.save()
+
+    def get_passnum(self):
+        return self.is_pass
