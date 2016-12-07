4f9d5e4e47d7bbb5dfdce30c29a725cc55e07df1
Chole_Liu <842007226@qq.com>
2016-12-07 11:47:29 +0800

    整合case的编辑，只使用一个接口

--- AutoTest/platform/datahandle/casedata.py
+++ AutoTest/platform/datahandle/casedata.py
@@ -59,7 +59,7 @@ def get_case_detail(data):
     data_case["api"] = data_api
     data_case["depnt_api"] = depnt_api
     del (data_case['_state'])
-    suite_list = list(CaseSuite.objects.all().filter(case_id=case_id).values_list("suite_id", flat=True))
+    suite_list = list(CaseSuite.objects.all().filter(case_id=case_id,case_type=1).values_list("suite_id", flat=True))
     data_case["suite_list"] = suite_list
     return {
         "code": 1,
@@ -79,61 +79,85 @@ def create_case(data):
                                   depnd_api_id=depnd_api_id, check_type=check_type).case_id
     suite_list_data = data['suite_list']
     for suite_id in suite_list_data:
-        CaseSuite.objects.create(pro_id=pro_id, api_id=api_id, case_id=case_id, suite_id=suite_id)
+        CaseSuite.objects.create(pro_id=pro_id, api_id=api_id, case_id=case_id, suite_id=suite_id,case_type=1)
     return {
         "code": 1,
         "msg": "保存成功"
     }
 
 
-def edit_case_info(data):
+def edit_case(data):
     case_id = data['case_id']
     pro_id = data['pro_id']
     api_id = data['api_id']
     case_desc = data['case_desc']
     case_name = data['case_name']
     depnd_api_id = data['depnd_api_id']
-    check_type = data['check_type']
-    Case.objects.all().filter(case_id=case_id).update(pro_id=pro_id, api_id=api_id, case_desc=case_desc,
-                                                      case_name=case_name, depnd_api_id=depnd_api_id,
-                                                      check_type=check_type)
-    CaseSuite.objects.all().filter(case_id=case_id).delete()
-    suite_list_data = data['suite_list']
-    for suite_id in suite_list_data:
-        CaseSuite.objects.create(pro_id=pro_id, api_id=api_id, case_id=case_id, suite_id=suite_id)
-    return {
-        "code": 1,
-        "msg": "保存成功"
-    }
-
-
-def edit_req(data):
-    case_id = data['case_id']
     input_data = data['input_data']
-    depnd_api_id = data['depnd_api_id']
-    Case.objects.all().filter(case_id=case_id).update(input_data=input_data, is_set=1, depnd_api_id=depnd_api_id)
-    return {
-        "code": 1,
-        "msg": "保存成功"
-    }
-
-
-def edit_resp(data):
-    case_id = data['case_id']
     exp_data = data['exp_data']
     case_schema = data['case_schema']
     check_type = data['check_type']
-    Case.objects.all().filter(case_id=case_id).update(exp_data=exp_data, check_type=check_type,case_schema=case_schema)
+    Case.objects.all().filter(case_id=case_id).update(
+        pro_id=pro_id, api_id=api_id, case_desc=case_desc,case_name=case_name, depnd_api_id=depnd_api_id,
+        check_type=check_type,input_data=input_data,exp_data=exp_data, case_schema=case_schema)
+    CaseSuite.objects.all().filter(case_id=case_id,case_type=1).delete()
+    suite_list_data = data['suite_list']
+    for suite_id in suite_list_data:
+        CaseSuite.objects.create(pro_id=pro_id, api_id=api_id, case_id=case_id, suite_id=suite_id,case_type=1)
     return {
-        "code": 1,
-        "msg": "保存成功"
-    }
+            "code": 1,
+            "msg": "保存成功"
+        }
 
+
+# def edit_case_info(data):
+#     case_id = data['case_id']
+#     pro_id = data['pro_id']
+#     api_id = data['api_id']
+#     case_desc = data['case_desc']
+#     case_name = data['case_name']
+#     depnd_api_id = data['depnd_api_id']
+#     check_type = data['check_type']
+#     Case.objects.all().filter(case_id=case_id).update(pro_id=pro_id, api_id=api_id, case_desc=case_desc,
+#                                                       case_name=case_name, depnd_api_id=depnd_api_id,
+#                                                       check_type=check_type)
+#     CaseSuite.objects.all().filter(case_id=case_id,case_type=1).delete()
+#     suite_list_data = data['suite_list']
+#     for suite_id in suite_list_data:
+#         CaseSuite.objects.create(pro_id=pro_id, api_id=api_id, case_id=case_id, suite_id=suite_id,case_type=1)
+#     return {
+#         "code": 1,
+#         "msg": "保存成功"
+#     }
+#
+#
+# def edit_req(data):
+#     case_id = data['case_id']
+#     input_data = data['input_data']
+#     depnd_api_id = data['depnd_api_id']
+#     Case.objects.all().filter(case_id=case_id).update(input_data=input_data, is_set=1, depnd_api_id=depnd_api_id)
+#     return {
+#         "code": 1,
+#         "msg": "保存成功"
+#     }
+#
+#
+# def edit_resp(data):
+#     case_id = data['case_id']
+#     exp_data = data['exp_data']
+#     case_schema = data['case_schema']
+#     check_type = data['check_type']
+#     Case.objects.all().filter(case_id=case_id).update(exp_data=exp_data, check_type=check_type,case_schema=case_schema)
+#     return {
+#         "code": 1,
+#         "msg": "保存成功"
+#     }
+#
 
 def del_case(data):
     case_id = data['case_id']
     Case.objects.all().get(case_id=case_id).delete()
-    CaseSuite.objects.all().filter(case_id=case_id).delete()
+    CaseSuite.objects.all().filter(case_id=case_id,case_type=1).delete()
     return {
         "code": 1,
         "msg": "删除成功"
