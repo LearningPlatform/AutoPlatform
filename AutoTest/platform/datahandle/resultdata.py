e9b4d3d7ede03b71bd0b856b36df5e8b99430448
Chole_Liu <842007226@qq.com>
2016-12-16 10:52:02 +0800

    修改结果详情，check_id为0时，给出默认值

--- AutoTest/platform/datahandle/resultdata.py
+++ AutoTest/platform/datahandle/resultdata.py
@@ -33,8 +33,14 @@ def get_result_detail_list(data):
         del (b['input_data'])
         c = jsontool.class_to_dict(Api.objects.all().get(api_id=a["api_id"]))
         if b["check_id"] != 0:
-            d = jsontool.class_to_dict(CheckModel.objects.all().get(check_id=b["check_id"]))
-            b = dict(b, **d)
+            checker = jsontool.class_to_dict(CheckModel.objects.all().get(check_id=b["check_id"]))
+        else:
+            checker = {
+                "check_name":"",
+                "check_desc":"默认",
+                "check_code":"",
+            }
+        b = dict(b, **checker)
         var = dict(a, **b)
         var = dict(var, **c)
         del (var['_state'])
@@ -59,9 +65,22 @@ def get_result_detail(data):
     b = jsontool.class_to_dict(Case.objects.all().get(case_id=a["case_id"]))
     del (b['input_data'])
     c = jsontool.class_to_dict(Api.objects.all().get(api_id=a["api_id"]))
+    if b["check_id"] != 0:
+        checker = jsontool.class_to_dict(CheckModel.objects.all().get(check_id=b["check_id"]))
+    else:
+        checker = {
+            "check_name": "",
+            "check_desc": "默认",
+            "check_code": "",
+        }
+    b = dict(b, **checker)
     var = dict(a, **b)
     var = dict(var, **c)
     del (var['_state'])
+    var["input_data"]["body"] = str(var["input_data"]["body"]).replace("\'", "\"")
+    var["input_data"]["url"] = str(var["input_data"]["url"]).replace("\'", "\"")
+    var["out_data"]["response_data"]["body"] = str(var["out_data"]["response_data"]["body"]).replace("\'", "\"")
+    var["out_data"]["response_data"]["header"] = str(var["out_data"]["response_data"]["header"]).replace("\'", "\"")
     return {
         "code": 1,
         "msg": "返回成功",
